be126ec915b3032b40a1b0570b6b431db1c4c1c3
AjiHardiNata <73481926+AjiHardiNata@users.noreply.github.com>
2022-05-13 15:01:35 +0700

    Update fakeaction.py

--- plugins/fakeaction.py
+++ plugins/fakeaction.py
@@ -4,45 +4,21 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+#
+# Ported by @mrismanaziz
+# @SharingUserbot
 
-"""
-✘ Commands Available -
-
-• `{i}ftyping <time/in secs>`
-    `Show Fake Typing in current chat.`
-
-• `{i}faudio <time/in secs>`
-    `Show Fake Recording Action in current chat.`
-
-• `{i}fvideo <time/in secs>`
-    `Show Fake video action in current chat.`
-
-• `{i}fgame <time/in secs>`
-    `Show Fake Game Playing Action in current chat.`
-
-• `{i}flocation <time/in secs>`
-    `Show Fake location Action in current chat.`
-
-• `{i}fcontact <time/in secs>`
-    `Show Fake contact choosing Action in current chat.`
-
-• `{i}fround <time/in secs>`
-    `Show Fake video message action in current chat.`
-
-• `{i}fphoto <time/in secs>`
-    `Show Fake sending photo in current chat.`
-
-• `{i}fdocument <time/in secs>`
-    `Show Fake sending document in current chat.`
-"""
+import asyncio
 import math
 import time
 
-from . import *
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import edit_delete, extract_time, man_cmd
 
 
-@ultroid_cmd(
-    pattern="f(typing|audio|contact|document|game|location|photo|round|video) ?(.*)"
+@man_cmd(
+    pattern="f(typing|audio|contact|document|game|location|photo|round|sticker|video) ?(.*)"
 )
 async def _(e):
     act = e.pattern_match.group(1)
@@ -52,9 +28,37 @@ async def _(e):
     if t.isdigit():
         t = int(t)
     elif t.endswith(("s", "h", "d", "m")):
-        t = math.ceil((await ban_time(e, t)) - time.time())
+        t = math.ceil((await extract_time(e, t)) - time.time())
     else:
         t = 60
-    await eor(e, f"Starting Fake Action For {t} sec.", time=5)
+    await edit_delete(e, f"**Memulai fake {act} selama** `{t}` **detik**", 3)
     async with e.client.action(e.chat_id, act):
         await asyncio.sleep(t)
+
+
+CMD_HELP.update(
+    {
+        "fakeaction": f"**Plugin :** `fakeaction`\
+        \n\n  •  **Syntax :** `{cmd}ftyping`  <jumlah detik>\
+        \n  •  **Function :** Menampilkan Pengetikan Palsu dalam obrolan\
+        \n\n  •  **Syntax :** `{cmd}faudio` <jumlah detik>\
+        \n  •  **Function :** Menampilkan Tindakan Merekam suara Palsu dalam obrolan\
+        \n\n  •  **Syntax :** `{cmd}fvideo` <jumlah detik>\
+        \n  •  **Function :** Menampilkan Tindakan Merekam Video Palsu dalam obrolan\
+        \n\n  •  **Syntax :** `{cmd}fround` <jumlah detik>\
+        \n  •  **Function :** Menampilkan Tindakan Merekam Live Video Round Palsu dalam obrolan\
+        \n\n  •  **Syntax :** `{cmd}fgame` <jumlah detik>\
+        \n  •  **Function :** Menampilkan sedang bermain game Palsu dalam obrolan\
+        \n\n  •  **Syntax :** `{cmd}fphoto` <jumlah detik>\
+        \n  •  **Function :** Menampilkan Tindakan Mengirim Photo Palsu dalam obrolan\
+        \n\n  •  **Syntax :** `{cmd}fdocument` <jumlah detik>\
+        \n  •  **Function :** Menampilkan Tindakan Mengirim Document/File Palsu dalam obrolan\
+        \n\n  •  **Syntax :** `{cmd}flocation` <jumlah detik>\
+        \n  •  **Function :** Menampilkan Tindakan Share Lokasi Palsu dalam obrolan\
+        \n\n  •  **Syntax :** `{cmd}fcontact` <jumlah detik>\
+        \n  •  **Function :** Menampilkan Tindakan Share Contact Palsu dalam obrolan\
+        \n\n  •  **Syntax :** `{cmd}fsticker` <jumlah detik>\
+        \n  •  **Function :** Menampilkan Tindakan Memilih Sticker Palsu dalam obrolan\
+    "
+    }
+)
